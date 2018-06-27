93beb098a3de35f2dbbedca6b5e2fd59f477ca7c
xiaoj-f <xiaoj-f@glodon.com>
2018-06-27 09:57:40 +0800

    mybatis project

---
+++ src/main/java/com/glodon/cg/mybatis/mybatis/Student.java
@@ -0,0 +1,34 @@
+package com.glodon.cg.mybatis.mybatis;
+
+public class Student {
+    private Integer id;
+    private String name;
+    private Double sal;
+
+    public Student() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSal() {
+        return sal;
+    }
+
+    public void setSal(Double sal) {
+        this.sal = sal;
+    }
+}
